7807e0347e44842c2b9c2236a71cbdbb96e6abd8
ayush-ahire <ayushgithub8@gmail.com>
2024-10-06 22:02:54 +0530

    fifth commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ interface IProps {
 
 const Home = ({ videos }: IProps) => {
   return (
-    <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh] mt-4">
+    <div className="xl:w-[1200px] m-auto  overflow-hidden h-[100vh] mt-4">
       <Navbar />
       <div className="flex gap-6 md:gap-20 ">
         <div className="h-[92vh] overflow-hidden  ">
